5cfb759463a9d8f1bf6dd48614a1a1b8fdec234c
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-02-15 01:32:56 -0600

    added attributions

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,13 @@
 console.log("Hello World, my name is Becca Buechle");
+
+// script to output simple string.
+
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Becca Buechle 
+; Date: 14 February 2020
+; Modified By: Becca Buechle
+; Description: Program to output a simple string of text.
+;===========================================
+*/
