76ca1ee5b9c9d732cd0278b322e1641c98446992
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-02-15 18:49:09 -0600

    primitive types assignment

---
+++ week-1/buechle-primitiveTypes.js
@@ -0,0 +1,22 @@
+var firstName = "Becca";
+var lastName = "Buechle";
+var age = "32";
+var street = "111 Foxtail Ln";
+var city = "Durango";
+var zipCode = "81301";
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+console.log (street);
+console.log (city);
+console.log (zipCode);
+
+/*
+============================================
+; Title: Assignment 1.4 Primitive Types
+; Author: Becca Buechle 
+; Date: 15 February 2020
+; Modified By: Becca Buechle
+; Description: Program practice primitive types.
+;===========================================
+*/
