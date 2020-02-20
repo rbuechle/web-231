6f18e2332604e51e00e119e8faf486288728e82e
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-02-19 23:58:08 -0600

    discussion board debugs

---
+++ week-1/Dinkins.js
@@ -0,0 +1,32 @@
+/*
+
+============================================
+
+; Title: Assignment 1.1
+
+; Author: Darius Dinkins
+
+; Date: 16 Feb 2020
+
+; Modified By: Becca Buechle
+
+; Description: This program demonstrates the
+
+; use of JavaScript types, values, and
+
+; and variables in an application.
+
+;===========================================
+
+*/
+
+var d; //This is variable "D"
+
+var house; //This is variable "house"
+
+var v12; //This is variable "v12"
+
+
+//Line 3 (var house declaration) contained the starting syntax for a multi-line comment (/*) which causes the rest of the code to be interpreted as a comment. /* removed.
+
+//Line 5 (var v12 declaration) incorrectly named the variable "12" instead of the intended "v12" as indicated by the comment. Variable name changed to v12.
