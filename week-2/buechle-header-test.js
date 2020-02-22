e1027d89cd840bd9ba878be5ab6f5b796ed2c573
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-02-22 01:22:44 -0600

    Initial Commit

---
+++ week-2/buechle-header-test.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Richard Krasso
+; Date: 22 February 2020
+; Modified By: Becca Buechle
+; Description: This program creates an alert telling
+the user hello world.
+;===========================================
+*/
+
+//Getting info from the header file
+const header = require('../buechle-header.js');
+
+//logging info to dev console first and last name and assignment name
+console.log (header.display("Rebecca", "Buechle", "2.2-Header"));
