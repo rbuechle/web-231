6abf68e17fd62192adf428957a09d87fb4d752b0
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-03-04 21:02:05 -0600

    3.1 discussion for Michelle Nesbitt

---
+++ week-3/3.1_nesbitt_discussion.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  discussion-3.1.js
+; Author: Michelle Nesbitt
+; Date:   27 February 2020
+; Modified By: Becca Buechle
+; Description: Sample of errors in Javascript
+;===========================================
+*/
+var myValue = 670;
+
+for (var i = 0; i > myValue; i++){
+  console.log(i);
+}
