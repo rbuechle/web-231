80e028df760c7f3b8d669a6ed219c4ac4beacbcc
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-03-04 21:29:50 -0600

    Comments and code corrections for Dinkins

---
+++ week-3/3.1_dinkins_discussion.js
@@ -0,0 +1,28 @@
+
+
+/*
+============================================
+; Title:  3.1 Discussion
+; Author: Darius Dinkins
+; Date:   04 March 2020
+; Modified By: Becca Buechle
+; Description: Discussion Post
+;===========================================
+*/
+
+var myAge = 30;
+
+
+if (myAge > 32) {
+  console.log("I'm older than thirty-two");
+} else {
+  console.log("I'm less than thirty-two");
+}
+console.log()
+
+//Side note- When you do your header each week you want to make sure to change the assignment and date and description. I've been forgetting as well. ( I made the correction so you can see how it should look)
+
+//1. So the first thing I noticed with your code is that else is spelled wrong.
+//2. you had an extra circle bracket after 32
+
+//also, for formatting consistency and to help prevent errors in your code you should use ; after each line in a code block.
