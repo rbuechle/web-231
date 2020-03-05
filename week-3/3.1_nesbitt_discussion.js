68842e485d9dc9f8573e41545a676371cbbce41c
Rebecca Buechle <rbuechle@my365.bellevue.edu>
2020-03-04 21:09:50 -0600

    added notes

--- week-3/3.1_nesbitt_discussion.js
+++ week-3/3.1_nesbitt_discussion.js
@@ -12,3 +12,12 @@ var myValue = 670;
 for (var i = 0; i > myValue; i++){
   console.log(i);
 }
+
+
+//1. The first thing I noticed was that you had commas instead of semicolon's by the zero and at the end of myValue
+
+//2. it wont log to the console log unless you use a less than sign
+
+//3. if the program actually worked it would continuously go down pretty much forever because of the i-- which doesn't really make any since because  the program will never be greater than 670.
+
+//Side note I got it to run to -20 thousand before it crashed.
